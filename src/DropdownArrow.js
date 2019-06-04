f4e2dfd977081552e2dc0962c21911c4440310d8
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-04 17:50:30 +0300

    styling

--- src/DropdownArrow.js
+++ src/DropdownArrow.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class DropdownArrow extends Component {
     render() {
         return (
-            <i className="fa fa-angle-down" onClick={this.props.toogle}></i>
+            <i className="fa fa-angle-down dropdown-arrow" onClick={this.props.toogle}></i>
         );
     }
 }
