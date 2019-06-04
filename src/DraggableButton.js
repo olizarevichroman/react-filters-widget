6f411e21703529ca6c29abe67230e5b5fc48ff95
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-04 17:46:06 +0300

    refactoring

--- src/DraggableButton.js
+++ src/DraggableButton.js
@@ -6,38 +6,31 @@ class DraggableButton extends Component {
     {
         super(props);
 
-        this.isDragged = false;
         this.onMouseDown = this.onMouseDown.bind(this);
         this.onMouseUp = this.onMouseUp.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
-        this.isDragged = false;
     }
 
     onMouseDown(pageX, pageY)
     {
         this.cursorX = pageX;
         this.cursorY = pageY;
-        this.isDragged = true;
 
         document.onmousemove = this.onMouseMove;
     }
 
     onMouseMove(e)
     {
-        if (this.isDragged)
-            {
-                let shiftX = e.pageX - this.cursorX;
-                let shiftY = e.pageY - this.cursorY;
-                this.cursorX = e.pageX;
-                this.cursorY = e.pageY;
-                this.props.shift(shiftX, shiftY);
-            }
+        let shiftX = e.pageX - this.cursorX;
+        let shiftY = e.pageY - this.cursorY;
+
+        this.cursorX = e.pageX;
+        this.cursorY = e.pageY;
+        this.props.shift(shiftX, shiftY);
     }
 
     onMouseUp()
     {
-        this.isDragged = false;
-
         document.onmousemove = null;
     }
 
@@ -46,7 +39,6 @@ class DraggableButton extends Component {
             <span className="fa fa-bars" 
             onMouseDown={(e) => this.onMouseDown(e.pageX, e.pageY)}
             onMouseUp={this.onMouseUp}
-            onDragStart={(e) => e.preventDefault()}
             />
         );
     }
