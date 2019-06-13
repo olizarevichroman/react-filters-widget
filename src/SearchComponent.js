909c2a5c580993de58d7e6a57a8ee5556370329a
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-13 16:40:47 +0300

    added debounce to the search component

--- src/SearchComponent.js
+++ src/SearchComponent.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import debounce from 'lodash.debounce';
 
 class SearchComponent extends Component {
 
-    //implement debounce for search
+    constructor(props)
+    {
+        super(props);
+
+        this.debouncedHandleChange = debounce(this.handleChange, 250);
+    }
+
+    handleChange(value)
+    {
+        console.log(value);
+    }
+
+    componentWillUnmount()
+    {
+        this.debouncedHandleChange.cancel();
+    }
 
     render() {
+
         return (
             <div className="searchWrapper">
                 <i className="fa fa-search"/>
-                <input type="text" placeholder="Find"/>
+                <input type = "text" placeholder = "Find" onChange = {(e) => this.debouncedHandleChange(e.target.value)}/>
             </div>
         );
     }
