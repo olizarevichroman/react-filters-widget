8114af7177aef84526fcebc09aa60ac29b717b47
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-14 12:36:20 +0300

    fixed styling and deleted useless action types

--- src/Actions/ActionTypes.js
+++ src/Actions/ActionTypes.js
@@ -3,8 +3,6 @@ const actionTypes =
     setFilterResults: "SET_FILTER_RESULTS",
     addTable: "ADD_TABLE",
     applyFilter: "APPLY_FILTER",
-    applySort: "APPLY_SORT",
-    cancelSort: "CANCEL_SORT",
     toggleRecord: "TOGGLE_RECORD",
     toggleTable: "TOGGLE_TABLE",
     toggleColumn: "TOGGLE_COLUMN",
