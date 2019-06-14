352745166d13361b771bbac3a7e38772212e4a17
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-14 15:30:42 +0300

    Updates

---
+++ precache-manifest.0c54d1126e2138996b8fb83d1c86617c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45938c6a874fd80928e5509010952806",
+    "url": "/react-filters-widget/index.html"
+  },
+  {
+    "revision": "0327924106e24be381f9",
+    "url": "/react-filters-widget/static/css/main.b85b665f.chunk.css"
+  },
+  {
+    "revision": "72dc1ab122940444b4ef",
+    "url": "/react-filters-widget/static/js/2.55114644.chunk.js"
+  },
+  {
+    "revision": "0327924106e24be381f9",
+    "url": "/react-filters-widget/static/js/main.01714f8a.chunk.js"
+  },
+  {
+    "revision": "5f346d5fd75b2476eb06",
+    "url": "/react-filters-widget/static/js/runtime~main.93680c01.js"
+  }
+]);
