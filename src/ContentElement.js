e28b69741a26a0b58fc9ebd0e89b1bed83dca088
Raman Alizarevich <Raman.Alizarevich@itechart-group.com>
2019-06-14 18:06:45 +0300

    fixed issue with bubble event from nested element(привет костыли)

--- src/ContentElement.js
+++ src/ContentElement.js
@@ -3,9 +3,19 @@ import Checkbox from './Checkbox';
 
 export default class ContentElement extends Component {
 
+    handleBubbleEvent(target)
+    {
+        if (target.localName === "input")
+        {
+            return;
+        }
+
+        this.props.onChange();
+    }
+
     render() {
         return (
-            <div className="content-element-container">
+            <div className="content-element-container" onClick = {(e) => this.handleBubbleEvent(e.target)}>
                 <Checkbox checked = {this.props.checked} onChange = {this.props.onChange}/>
                 <span className = "content-element">{this.props.text}</span>
             </div>
